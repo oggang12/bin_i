e05fa204ddcfe738923179e1d11c889d0cc66bb7
oggang12 <167792974+oggang12@users.noreply.github.com>
2024-04-22 23:38:18 +0530

    Add files via upload

---
+++ Mprj/parallel_quicksort.cpp
@@ -0,0 +1,70 @@
+#include <iostream>  
+#include<thread>
+#include<mutex>
+
+std::mutex mt;
+
+using namespace std;  
+  
+/* function that consider last element as pivot,  
+place the pivot at its exact position, and place  
+smaller elements to left of pivot and greater  
+elements to right of pivot.  */  
+int partition (int a[], int start, int end)  
+{  
+    int pivot = a[end]; // pivot element  
+    int i = (start - 1);  
+  
+    for (int j = start; j <= end - 1; j++)  
+    {  
+        // If current element is smaller than the pivot  
+        if (a[j] < pivot)  
+        {  
+            i++; // increment index of smaller element  
+            int t = a[i];  
+            a[i] = a[j];  
+            a[j] = t;  
+        }  
+    }  
+    int t = a[i+1];  
+    a[i+1] = a[end];  
+    a[end] = t;  
+    return (i + 1);  
+}  
+  
+/* function to implement quick sort */  
+void quick(int a[], int start, int end) /* a[] = array to be sorted, start = Starting index, end = Ending index */  
+{  
+    if (start < end)  
+    {  
+        int p = partition(a, start, end);  //p is the partitioning index  
+        quick(a, start, p - 1);  
+        quick(a, p + 1, end);  
+    }  
+}  
+  
+/* function to print an array */  
+void printArr(int a[], int n)  
+{  
+    int i;  
+    std::lock_guard<std::mutex> lg(mt);
+    for (i = 0; i < n; i++)  
+        cout<<a[i]<< " ";  
+} 
+
+int main()  
+{  
+    int a[] = { 23, 8, 28, 13, 18, 26 };  
+    int n = sizeof(a) / sizeof(a[0]);  
+    int x=n-1;
+    cout<<"Before sorting array elements are - \n";  
+    printArr(a,n);
+    std::thread t1(quick,std::ref(a), 0, std::ref(x));  
+    cout<<"\nAfter sorting array elements are - \n";    
+    std::thread t2(printArr,std::ref(a), std::ref(n));  
+
+    if(t1.joinable()){t1.join();}
+    if(t2.joinable()){t2.join();}
+      
+    return 0;  
+}  
